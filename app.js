e13f584968ffeb03d42390381caaa7be71f746bf
Alperen Yılmaz <106971801+alperenylmz@users.noreply.github.com>
2026-02-02 12:41:40 +0300

    Side Weights Calculations & Presentation

--- app.js
+++ app.js
@@ -16,6 +16,11 @@
     const logScrollElement = document.getElementById("log");
     const clearLogButton = document.getElementById("clearLogs");
 
+    //istatistik verilerimiz için
+    const statLeftEl = document.getElementById("statLeft");
+    const statAngleEl = document.getElementById("statAngle");
+    const statRightEl = document.getElementById("statRight");
+
     function appendLogLine(text) {
         if (!logListElement) return;
       
@@ -77,17 +82,67 @@
         return Math.max(-30, Math.min(30, (rightTorque - leftTorque) / 10));
     }
 
+    //stat göstermek için
+    function calculateSideWeights(items){
+        let leftWeight=0;
+        let rightWeight=0;
+
+        for(const item of items){
+            if(item.x<PIVOT){
+                leftWeight+=item.weight;
+            }else{
+                rightWeight+=item.weight;
+            }
+        }
+        return {leftWeight,rightWeight};
+    }
+
     //rotasyon renderı için
     function renderAngle(angleDegree){
         plankElement.style.transform=`translateX(-50%) rotate(${angleDegree}deg)`;
     }
 
+    //item layer
+    function getOrCreateItemsLayer(){
+        let layer=plankElement.querySelector(".itemsLayer");
+        if(!layer){
+            layer=document.createElement("div");
+            layer.className="itemsLayer";
+            plankElement.appendChild(layer);
+        }
+        return layer;
+    }
+
+    //element oluşturup layera ekliyoruz
+    function renderItems(){
+        const layer=getOrCreateItemsLayer();
+        layer.innerHTML="";
+
+        for(const item of state.items){
+            const el=document.createElement("div");
+            el.className="seesawItem";
+            el.dataset.id=item.id;
+
+            const leftPercent=(item.x/PLANK_LENGTH)*100; //ilgili noktaya yerleştirmek için plank uzunluğu ile yüzdelik aldık
+            el.style.left=`${leftPercent}%`;
+
+            el.textContent=`${item.weight}kg`; //daire içindeki weight verisi
+            layer.appendChild(el);
+        }
+    }
+
     //state değişikliklerini render ediyoruz
     function updateSeesaw(){
         const {leftTorque, rightTorque}=calculateTorque(state.items);
+        const {leftWeight,rightWeight}=calculateSideWeights(state.items);
         const angle=calculateAngle(leftTorque, rightTorque);
 
         renderAngle(angle);
+        renderItems();
+
+        if (statLeftEl) statLeftEl.textContent = `${leftWeight}kg`;
+        if (statAngleEl) statAngleEl.textContent = `${angle.toFixed(2)}°`;
+        if (statRightEl) statRightEl.textContent = `${rightWeight}kg`;
 
         //log güncellemeis
         appendLogLine(`Torque L=${leftTorque.toFixed(1)} | R=${rightTorque.toFixed(1)} → angle=${angle.toFixed(2)}°`);
